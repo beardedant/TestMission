7a8e4c24a256d462fe123cd88f48f37b35ee944f
ant <theant@mail.ru>
2022-07-05 17:44:16 +0400

    Stop branch. Architectural error

--- app/src/main/java/com/testmission/MainActivity.kt
+++ app/src/main/java/com/testmission/MainActivity.kt
@@ -5,16 +5,20 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.AttributeSet
 import android.view.View
+import android.widget.Toast
+import com.testmission.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_main)
-    }
 
-    override fun onCreateView(name: String, context: Context, attrs: AttributeSet): View? {
-        return super.onCreateView(name, context, attrs)
-
-
+//        supportFragmentManager
+//            .beginTransaction()
+//            .replace(R.id.fragment_container, ClientListFragment.newInstance())
+//            .commit()
+//    }
     }
 }
