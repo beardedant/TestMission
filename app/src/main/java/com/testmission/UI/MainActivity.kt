e971d14c7e585451b72fe4b4756594f225766c44
ant <theant@mail.ru>
2022-07-05 17:50:17 +0400

    Initial commit

--- app/src/main/java/com/testmission/UI/MainActivity.kt
+++ app/src/main/java/com/testmission/UI/MainActivity.kt
@@ -1,11 +1,12 @@
-package com.testmission
+package com.testmission.UI
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.testmission.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.fragment_main)
     }
 }
---
+++ app/src/main/java/com/testmission/UI/MainFragment.kt
@@ -0,0 +1,6 @@
+package com.testmission.UI
+
+import androidx.fragment.app.Fragment
+
+class MainFragment:Fragment() {
+}
